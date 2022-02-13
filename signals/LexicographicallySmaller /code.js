656a5300c2900e5dcf9c34c235a7aa7167250644
Daniel Silion <siliond@gmail.com>
2022-02-13 08:10:47 -0600

    Lexicographically Smaller

---
+++ signals/LexicographicallySmaller /code.js
@@ -0,0 +1,15 @@
+function solution(s, t) {
+    let result = 0;
+    
+    result = s<t;
+    
+    return result;
+}
+
+log(`Input:
+s: "ab12c"
+t: "1zz456"
+
+Expected Output:
+1`)
+solution ("ab12c", "1zz456");
