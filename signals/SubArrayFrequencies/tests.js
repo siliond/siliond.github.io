f0d1a58f054308ad2a1bf20bce64c238aa7c7142
Daniel Silion <siliond@gmail.com>
2022-02-13 19:32:54 -0600

    extra test

--- signals/SubArrayFrequencies/tests.js
+++ signals/SubArrayFrequencies/tests.js
@@ -6,4 +6,12 @@ describe("SubArray Frequencies suite", function() {
     [1]`, function() {
       expect(solution ([1, 2], 2)).toEqual([1]);
     });
+
+    it(`Input:
+    arr: [1, 3, 2, 2, 3]
+    m: 4
+    Expected Output:
+    [2, 2]`, function() {
+      expect(solution ([1, 3, 2, 2, 3], 4)).toEqual([2, 2]);
+    });
   });
