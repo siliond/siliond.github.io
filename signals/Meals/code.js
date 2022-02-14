858c6b789f6b18d341378484df7e02e19951f784
Daniel Silion <siliond@gmail.com>
2022-02-14 16:29:17 -0600

    prelim

--- signals/Meals/code.js
+++ signals/Meals/code.js
@@ -1,5 +1,23 @@
 function solution(cntProducts, quantities, costs, meals) {
     let mealMinCosts = [];
 
+    for (let i=0;i<meals.length; i++) {
+        for (let j=0; j<cntProducts; j++) {
+            let cntProduct = meals[i][j];
+
+            let mealMinCost = getMealMinCost(j, cntProduct, quantities, costs);
+        }
+    }
+
     return mealMinCosts;
+}
+
+function getMealMinCost(noProduct, cntProduct, quantities, costs) {
+    let mealMinCost = 0;
+
+    for (let i=0;i<quantities.length; i++) {
+        let [quantity, cost] = [quantities[i][noProduct], costs[i][noProduct]];
+    }
+
+    return mealMinCost;
 }
