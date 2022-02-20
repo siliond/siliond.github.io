12fa16f05e68ff78a0c5e5cf3ba2a31e2509de93
Daniel Silion <siliond@gmail.com>
2022-02-20 07:35:13 -0600

    fix

--- signals/Minesweeper/code.js
+++ signals/Minesweeper/code.js
@@ -1,5 +1,5 @@
 function solution(field, x, y) {
-    let state = new Array.from(field, a => (Array.from(a, v => -1)));
+    let state = new Array.from(field, a => Array.from(a, v => -1));
 
     return state;
 }
