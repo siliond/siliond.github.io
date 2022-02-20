3c93c75bdc75ad9b357308ca5e146ed2aba7302c
Daniel Silion <siliond@gmail.com>
2022-02-20 07:44:25 -0600

    fix

--- signals/Minesweeper/code.js
+++ signals/Minesweeper/code.js
@@ -2,7 +2,7 @@ function solution(field, x, y) {
     let noMines = 0;
 
     let state = Array.from(field, (a, iX) => Array.from(a, (v, iY) => 
-        (x==iX&&y==iY)?-1:noMines
+        x==iX&&y==iY?noMines:-1
         ));
 
     return state;
