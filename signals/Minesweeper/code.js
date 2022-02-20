3050e913e1683f7c7025f09ccd8dd62c88f69864
Daniel Silion <siliond@gmail.com>
2022-02-20 09:01:13 -0600

    recursion

--- signals/Minesweeper/code.js
+++ signals/Minesweeper/code.js
@@ -1,19 +1,30 @@
 function solution(field, x, y) {
+    let state = Array.from(field, (a, iX) => Array.from(a, (v, iY) => -1));
+
+    return processNeighbors(field, x, y, state);
+}
+
+function processNeighbors(field, x, y, state) {
     let noMines = 0;
     let neighbors = [];
 
-    let state = Array.from(field, (a, iX) => Array.from(a, (v, iY) => 
-        {
-            if (iX>=x-1&&iX<=x+1&&iY>=y-1&&iY<=y+1) {
-                if (v&&(x!==iX||y!==iY))
+    for (let iX=(x-1>=0)?x-1:0;iX<=(x+1<field.length?x+1:field.length);iX++) {
+        for (let iY=(y-1>=0)?y-1:0;iY<=(y+1<field[iX].length?y+1:field[iX].length);iY++) {
+            if (x!==iX||y!==iY) {
+                if (v)
                     noMines++;
+                neighbors.push(iX, iY);
             }
-
-            return -1;
         }
-        ));
+    }
 
     state[x][y] = noMines;
+
+    if (noMines == 0) {
+        for (let i=0; i<neighbors.length; i++) {
+            state = processNeighbors(field, x, y, state);
+        }
+    }
 
     return state;
 }
