ad0d9e9c7a423e9fb55e799800102aedcce7aef8
Daniel Silion <siliond@gmail.com>
2022-12-03 06:36:57 -0600

    Create TwitterShare.js

---
+++ scripts/TwitterShare.js
@@ -0,0 +1,10 @@
+function TwitterShare() {
+var selectedText = window.getSelection().toString();
+var twitterUrl = "https://twitter.com/share?url=<URL>&text=" + encodeURIComponent(selectedText) + "&via=<USERNAME>&hashtags=<HASHTAGS>";
+window.open(twitterUrl, "_blank");
+
+var selectedText = window.getSelection().toString();
+var currentUrl = encodeURIComponent(window.location.href);
+var twitterUrl = "https://twitter.com/share?url=" + currentUrl + "&text=" + encodeURIComponent(selectedText) + "&via=<USERNAME>&hashtags=<HASHTAGS>";
+window.open(twitterUrl, "_blank");
+}
