402f8e77f06dd6b9fd0bafc28e66c5cad9898454
Daniel Silion <siliond@gmail.com>
2022-12-11 09:41:12 -0600

    getJSONParam

--- signals/webrtc/answering.js
+++ signals/webrtc/answering.js
@@ -1,57 +1,68 @@
+function getJSONParam() {
+    // Get the current URL
+    const url = new URL(window.location.href);
+
+    // Get the value of the "id" parameter from the URL
+    const json = url.searchParams.get('json');
+
+    textoffer.value = json;
+    clickofferpasted();
+}
+
 function clickofferpasted() {
-  console.log('clickremoteoffer');
-  document.getElementById('buttonofferpasted').disabled = true;
-  peerConnection = createPeerConnection(lasticecandidate);
-  peerConnection.ondatachannel = handledatachannel;
-  textelement = document.getElementById('textoffer');
-  textelement.readOnly = true;
-  offer = JSON.parse(textelement.value);
-  setRemotePromise = peerConnection.setRemoteDescription(offer);
-  setRemotePromise.then(setRemoteDone, setRemoteFailed);
+    console.log('clickremoteoffer');
+    document.getElementById('buttonofferpasted').disabled = true;
+    peerConnection = createPeerConnection(lasticecandidate);
+    peerConnection.ondatachannel = handledatachannel;
+    textelement = document.getElementById('textoffer');
+    textelement.readOnly = true;
+    offer = JSON.parse(textelement.value);
+    setRemotePromise = peerConnection.setRemoteDescription(offer);
+    setRemotePromise.then(setRemoteDone, setRemoteFailed);
 }
 
 function setRemoteDone() {
-  console.log('setRemoteDone');
-  createAnswerPromise = peerConnection.createAnswer();
-  createAnswerPromise.then(createAnswerDone, createAnswerFailed);
+    console.log('setRemoteDone');
+    createAnswerPromise = peerConnection.createAnswer();
+    createAnswerPromise.then(createAnswerDone, createAnswerFailed);
 }
 
 function setRemoteFailed(reason) {
-  console.log('setRemoteFailed');
-  console.log(reason);
+    console.log('setRemoteFailed');
+    console.log(reason);
 }
 
 function createAnswerDone(answer) {
-  console.log('createAnswerDone');
-  setLocalPromise = peerConnection.setLocalDescription(answer);
-  setLocalPromise.then(setLocalDone, setLocalFailed);
-  document.getElementById('spananswer').classList.toggle('invisible');
+    console.log('createAnswerDone');
+    setLocalPromise = peerConnection.setLocalDescription(answer);
+    setLocalPromise.then(setLocalDone, setLocalFailed);
+    document.getElementById('spananswer').classList.toggle('invisible');
 }
 
 function createAnswerFailed(reason) {
-  console.log('createAnswerFailed');
-  console.log(reason);
+    console.log('createAnswerFailed');
+    console.log(reason);
 }
 
 function setLocalDone() {
-  console.log('setLocalDone');
+    console.log('setLocalDone');
 }
 
 function setLocalFailed(reason) {
-  console.log('setLocalFailed');
-  console.log(reason);
+    console.log('setLocalFailed');
+    console.log(reason);
 }
 
 function lasticecandidate() {
-  console.log('lasticecandidate');
-  textelement = document.getElementById('textanswer');
-  answer = peerConnection.localDescription
-  textelement.value = JSON.stringify(answer);
+    console.log('lasticecandidate');
+    textelement = document.getElementById('textanswer');
+    answer = peerConnection.localDescription
+    textelement.value = JSON.stringify(answer);
 }
 
 function handledatachannel(event) {
-  console.log('handledatachannel');
-  dataChannel = event.channel;
-  dataChannel.onopen = datachannelopen;
-  dataChannel.onmessage = datachannelmessage;
+    console.log('handledatachannel');
+    dataChannel = event.channel;
+    dataChannel.onopen = datachannelopen;
+    dataChannel.onmessage = datachannelmessage;
 }
