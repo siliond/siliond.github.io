b31e4daf5dddf2e4b88a940ac4a13b6337ed07dd
Daniel Silion <siliond@gmail.com>
2022-12-12 11:04:47 -0600

    Update answering.js

--- signals/webrtc/answering.js
+++ signals/webrtc/answering.js
@@ -7,6 +7,58 @@ Yes, in a WebRTC implementation, after a client accepts an offer, they can send 
 Daniel Silion
 How would you send the answer?
 After the local description has been set, the client can send the answer to the other client using the appropriate signaling mechanism. This typically involves using a signaling server to exchange messages between the clients. For example, the client could use the send() method of the RTCPeerConnection object to send the answer to the signaling server, which would then forward the message to the other client. The other client could then use the onmessage event of the RTCPeerConnection object to receive the answer and process it.
+
+In a LOCAL (no stun) Webrtc implementation is there a way to pass the answer to the other client after accepting the offer
+Yes, in a WebRTC implementation, after one client sends an offer to another client, the second client can accept the offer by creating an answer and sending it back to the first client. This is done using the createAnswer() method, which is part of the WebRTC API. Here is an example of how it might be used:
+
+Copy code
+// First, get the local audio and video tracks
+const audioTrack = getLocalAudioTrack();
+const videoTrack = getLocalVideoTrack();
+
+// Create a new PeerConnection
+const pc = new RTCPeerConnection();
+
+// Add the local audio and video tracks to the PeerConnection
+pc.addTrack(audioTrack, videoTrack);
+
+// When the remote client sends an offer, handle it by creating an answer
+pc.onicecandidate = (event) => {
+  if (event.candidate) {
+    // Send the ICE candidate to the remote client
+    sendToRemoteClient(event.candidate);
+  }
+};
+
+pc.ontrack = (event) => {
+  // When the remote client adds a track, display it on the page
+  displayRemoteTrack(event.track);
+};
+
+pc.onnegotiationneeded = async () => {
+  // When negotiation is needed, create an answer
+  const offer = await pc.createOffer();
+  await pc.setLocalDescription(offer);
+
+  // Send the offer to the remote client
+  sendToRemoteClient(offer);
+};
+
+// When the remote client sends an offer, accept it by creating an answer
+pc.onnegotiationneeded = async () => {
+  // When the remote client sends an offer, create an answer
+  const answer = await pc.createAnswer();
+  await pc.setLocalDescription(answer);
+
+  // Send the answer back to the remote client
+  sendToRemoteClient(answer);
+};
+
+// When the remote client adds a track, display it on the page
+pc.ontrack = (event) => {
+  displayRemoteTrack(event.track);
+};
+This is just one way to implement the process of sending and receiving offers and answers in WebRTC. The exact details of how you implement this will depend on your specific use case and the details of your WebRTC implementation.
 */
 
 
