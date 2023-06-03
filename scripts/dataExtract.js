35d80b6763c806a7dd5b228f8439ce1f9cf4b021
Daniel Silion <siliond@gmail.com>
2023-06-03 06:27:47 -0500

    Copy button on right page only

--- scripts/dataExtract.js
+++ scripts/dataExtract.js
@@ -4,27 +4,29 @@ function createCopyToClipboard(text) {
 		</button>
 	*/
 
-    const btn = document.createElement('button');
-    btn.classList.add("copyButton");
+    if (text) {
+        const btn = document.createElement('button');
+        btn.classList.add("copyButton");
 
-    btn.setAttribute('data-clipboard-text', text);
+        btn.setAttribute('data-clipboard-text', text);
 
-    btn.style.position = 'absolute';
-    btn.style.backgroundColor = 'lawngreen';
-    btn.style.cursor = 'pointer';
-    btn.style.zIndex = '2147483647';
+        btn.style.position = 'absolute';
+        btn.style.backgroundColor = 'lawngreen';
+        btn.style.cursor = 'pointer';
+        btn.style.zIndex = '2147483647';
 
-    btn.style.left = '10px';
-    btn.style.top = '10px';
-    document.body.appendChild(btn);
-    btn.innerText = "Copy";
+        btn.style.left = '10px';
+        btn.style.top = '10px';
+        document.body.appendChild(btn);
+        btn.innerText = "Copy";
+    }
 }
 
 function copyOnLoad() {
     setTimeout(() => {
         var values = valueExpressions.map((value) => (typeof(value) == 'function' ? value() : eval(value)).trim());
 
-        createCopyToClipboard(values.join("\t"));
+        createCopyToClipboard(values.join("\t").trim());
 
         var clipboard = new ClipboardJS('.copyButton');
     }, 3000);
